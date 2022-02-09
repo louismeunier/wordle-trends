8d85fbc24da2b1cca2290e9d73c9b25d54a3e1e4
louismeunier <louismeunier123@gmail.com>
2022-02-09 14:09:14 -0500

    Add description file

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-Rscript -e "install.packages('rsconnect')"
 Rscript -e "rsconnect::setAccountInfo(name='$NAME', token='$TOKEN', secret='$SECRET')"
 Rscript -e "rsconnect::deployApp(appName='wordle-trends')"
