d410154c0edf99fc9386b49d82dee11ac36fe4ce
louismeunier <louismeunier123@gmail.com>
2022-02-09 17:57:04 -0500

    Syntax fix

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,4 +1,4 @@
-Rscript -e "install.packages('rsconnect', repos='http://cran.us.r-project.org, type='binary')"
+Rscript -e "install.packages('rsconnect', repos='http://cran.us.r-project.org', type='binary')"
 
 for index in `sed -rn 's/(.*library\()([^,]+)(\).*)/\2/p' app.R`
 do
