4e7cee5a6b8969349623ed668fb3347cd8ef95d3
louismeunier <louismeunier123@gmail.com>
2022-02-09 17:53:51 -0500

    Add typpe binary to deps

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,7 +1,7 @@
-Rscript -e "install.packages('rsconnect')"
+Rscript -e "install.packages('rsconnect', repos='http://cran.us.r-project.org, type='binary')"
 
 for index in `sed -rn 's/(.*library\()([^,]+)(\).*)/\2/p' app.R`
 do
     echo "Installing $index"
-    Rscript -e "install.packages($index, repos='http://cran.us.r-project.org')"
+    Rscript -e "install.packages($index, repos='http://cran.us.r-project.org', type='binary')"
 done
