3f0925e133a99ac1dba8ebeb7919d97fbaa4acef
louismeunier <louismeunier123@gmail.com>
2022-02-09 13:42:41 -0500

    Install deps when publishing

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+Rscript -e "install.packages('rsconnect')"
 Rscript -e "rsconnect::setAccountInfo(name='$NAME', token='$TOKEN', secret='$SECRET')"
 Rscript -e "rsconnect::deployApp(appName='wordle-trends')"
