ff54e89faf1dcfefd7dab8db2735b813bce72b8f
louismeunier <louismeunier123@gmail.com>
2022-02-09 18:18:48 -0500

    Use R shell rather than shell

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,7 +1,7 @@
-Rscript -e "install.packages('rsconnect', repos='http://cran.us.r-project.org', type='binary')"
+Rscript -e "install.packages('rsconnect', repos='http://cran.us.r-project.org')"
 
 for index in `sed -rn 's/(.*library\()([^,]+)(\).*)/\2/p' app.R`
 do
     echo "Installing $index"
-    Rscript -e "install.packages($index, repos='http://cran.us.r-project.org', type='binary')"
+    Rscript -e "install.packages($index, repos='http://cran.us.r-project.org')"
 done
